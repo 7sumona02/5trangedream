326c04fc7b60305d339441472ca85af8de042384
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-10-27 22:24:38 +0530

    added

---
+++ app/lab/(lab)/layout.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function LabLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
