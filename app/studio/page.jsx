39b48f75871f6ad56490b2732ff3d5ae4db6bf3c
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-11-10 20:12:48 +0530

    studio responsive

--- app/studio/page.jsx
+++ app/studio/page.jsx
@@ -7,8 +7,8 @@ const hoverStyle = "hover:text-black hover:bg-[#dddedf] cursor-pointer"
 const page = () => {
     return (
         <SmoothFadeLayout>
-            <Container className='font-mono text-[#dddedf]'>
-                <div className='flex justify-between items-start'>
+            <Container className='font-mono text-[#dddedf] md:px-0 px-5'>
+                <div className='flex md:flex-row flex-col justify-between items-start'>
                     <div>
                         <Link href='/' className='text-lg cursor-pointer'>Sumona Biswas</Link>
                         <div className='text-sm max-w-lg'>
@@ -70,8 +70,8 @@ const Project = ({ title, type, link }) => {
 
 const Contact = () => {
     return (
-        <div className="min-w-[80%] h-100 mt-20">
-            <div className="bg-[#dddedf] w-40 h-10 text-sm flex justify-center items-center ml-15 relative rounded-t-xl text-black">
+        <div className="w-full h-100 mt-20">
+            <div className="bg-[#dddedf] w-40 h-10 text-sm flex justify-center items-center md:ml-15 ml-10 relative rounded-t-xl text-black">
                 <div className="absolute size-10 bg-[#5652f5] rounded-full bottom-0 -left-10 z-10"></div>
                 <div className="absolute size-10 bg-[#5652f5] rounded-full bottom-0 -right-10 z-10"></div>
                 <div className="absolute w-5 h-5 bg-[#dddedf] bottom-0 -left-4.5"></div>
@@ -79,12 +79,12 @@ const Contact = () => {
                 Contact
             </div>
             <div className="h-90 w-full bg-[#dddedf] rounded-sm border border-[#dddedf] flex flex-col justify-center">
-               <div className="max-w-lg mx-auto">
-                 <p className="text-black text-sm max-w-lg">If you're an artist, designer, or brand building something beautiful. I'd love to help you shape your online presence.
+               <div className="max-w-lg mx-auto md:px-0 px-5">
+                 <p className="text-black text-sm">If you're an artist, designer, or brand building something beautiful. I'd love to help you shape your online presence.
                     Let's create something timeless, together.
                 </p>
-                <div className="text-sm py-2 border-y border-y-black text-black mt-10">
-                Get in touch: <Link href='/' className="ml-5">@sumonadotwork@gmail.com</Link>
+                <div className="text-sm py-2 border-y border-y-black text-black mt-10 flex md:flex-row flex-col md:items-center md:gap-5">
+                <div>Get in touch: </div><Link href='/'>@sumonadotwork@gmail.com</Link>
                 </div>
                </div>
             </div>
