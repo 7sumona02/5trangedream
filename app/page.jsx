48603c24efa58a2a3b49d9146fb3b62ac8526d12
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-11-12 11:17:01 +0530

    added

--- app/page.jsx
+++ app/page.jsx
@@ -18,6 +18,8 @@ export default function Page() {
     { name: "Ticker", link: "https://ticker-next.vercel.app/" },
     { name: "Yoake Studio", link: "https://yoake-studio.vercel.app/" },
     { name: "Code Canvas", link: "https://code-canvas-neon.vercel.app/" },
+    { name: "Launch", link: "https://sumona.me/ui/launch" },
+    { name: "Portfolio", link: "https://sumona.me/ui/portfolio" },
   ];
 
   const socials = [
