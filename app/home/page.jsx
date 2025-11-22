4e1713cf274ac0c10859289cea280f5f260f4801
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-11-22 18:40:55 +0530

    added

---
+++ app/home/page.jsx
@@ -0,0 +1,114 @@
+import DitherTrail from "@/components/Cursor";
+import SmoothFadeLayout from "@/components/SmoothFadePageTransition"
+import Link from "next/link";
+import Time from '@/components/Time'
+
+export default function Page() {
+  const hoverStyle = "hover:text-black hover:bg-[#dddedf] cursor-pointer"
+
+  const works = [
+    { title: "Free Gluten", role: "Freelance Developer", time: "2025", link: "https://www.freegluten2025.com/" },
+    { title: "FuturixAI", role: "Frontend Developer", time: "Oct/25", link: "https://www.futurixai.com/" },
+    { title: "CVNT", role: "Tech Intern", time: "Jun/25", link: "https://www.linkedin.com/company/wearcvnt/" },
+    { title: "Veridia.io", role: "Designer", time: "Oct/25", link: "https://www.linkedin.com/company/veridia-io/posts/?feedView=all" },
+  ]
+
+  const projects = [
+    { name: "Linkspace", link: "https://linkspace.sumona.me/" },
+    { name: "Ticker", link: "https://ticker-next.vercel.app/" },
+    { name: "Yoake Studio", link: "https://yoake-studio.vercel.app/" },
+    { name: "Code Canvas", link: "https://code-canvas-neon.vercel.app/" },
+    { name: "Launch", link: "/ui/launch" },
+    { name: "Portfolio", link: "/ui/portfolio" },
+  ];
+
+  const socials = [
+    { name: "X", link: "https://x.com/weebdev_san" },
+    { name: "Github", link: "https://github.com/7sumona02" },
+    { name: "Linkedin", link: "https://linkedin.com/in/sumona-biswas" },
+    { name: "Email", link: "mailto:sumonadotwork@gmail.com" },
+    { name: "Resume", link: "https://drive.google.com/file/d/17aVV7crExybOJfKU4XK6leP2EXkCtFf7/view?usp=sharing" }, 
+  ];
+
+  return (
+    <div className="md:h-screen min-h-dvh flex justify-center items-center bg-[#5652f5] selection:text-black selection:bg-[#dddedf] overflow-hidden py-5">
+      <DitherTrail
+        className="w-screen h-screen fixed inset-0 pointer-events-none"
+        trailColor="#dddedf"
+        dotSize={20}
+        fadeDuration={400}
+      />
+      <SmoothFadeLayout className="flex justify-center items-center">
+        <div className="flex flex-col md:p-10 p-5 font-mono text-[#dddedf] font-medium text-sm md:w-xl">
+          <header className="flex justify-between w-full items-end">
+            <span>Sumona Biswas</span>
+            <span className="text-xs"><Time /></span>
+          </header>
+
+          <section className="pt-5 space-y-1">
+            <p>Design Engineer, Founder of <Link href='/pix-studio' className={`${hoverStyle} underline`}>Pix Studio</Link></p>
+            <p>
+              Find my UI experiments at{" "}
+              <Link href='/lab' className={`${hoverStyle} underline`}>lab</Link>.
+            </p>
+            <p>
+              I also{" "}
+              <Link href='/blog' className={`${hoverStyle} underline`}>write</Link>.
+            </p>
+          </section>
+
+          <section className="pt-5 w-full">
+            <h2>{`{Works}`}</h2>
+            <div className="pt-3 space-y-1">
+              {works.map(({ role, title, time, link }) => (
+                <div
+                  key={title}
+                  className="flex justify-between items-center flex-nowrap text-sm"
+                >
+                  <div className="truncate">
+                    ↳ {role} at{" "}
+                    <Link href={link} target="_blank" className={`${hoverStyle} underline`}>{title}</Link>
+                  </div>
+                  <div className="ml-2 shrink-0 text-right">~ {time}</div>
+                </div>
+              ))}
+            </div>
+          </section>
+
+          <section className="pt-5">
+            <h2>{`{Projects}`}</h2>
+            <div className="pt-3 space-y-0.5">
+              {projects.map(({ name, link }) => (
+                <div key={name}>
+                  ↳ <Link href={link} className={`${hoverStyle} underline`} target="_blank">
+                    {name}
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </section>
+
+          <section className="pt-5 w-full">
+            <h2>{`{OpenSource}`}</h2>
+            <div className="pt-3 space-y-1">
+              <Link href='/blog/lexit'>↳ <span className={`${hoverStyle} underline`}>Lexit</span></Link>
+            </div>
+          </section>
+
+          <section className="pt-5">
+            <h2>{`{Online}`}</h2>
+            <div className="pt-3 space-y-0.5">
+              {socials.map(({ name, link }) => (
+                <div key={name}>
+                  ↳ <Link href={link} target="_blank" className={`${hoverStyle} underline`}>
+                    {name}
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </section>
+        </div>
+      </SmoothFadeLayout>
+    </div>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
-import { Cormorant_Garamond, Geist, Geist_Mono } from "next/font/google";
+import { Cormorant_Garamond, Geist, Geist_Mono, Inconsolata } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const cormorant = Cormorant_Garamond({
+const inconsolata = Inconsolata({
   variable: "--font-geist-sans",
   subsets: ["latin"],
   weight: ["400","500","600"]
@@ -31,8 +32,21 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${cormorant.className} antialiased cursor-pixel`}
+        className={`${geistSans.variable} ${geistMono.variable} ${inconsolata.className} antialiased cursor-pixel`}
       >
+         <Toaster
+         toastOptions={{
+          style: {
+            background: "#dddedf",             // toast bg
+            color: "#000",                  // text color
+            borderRadius: "0px",
+            border: "1px solid #000",
+            boxShadow: 'none',
+            padding: "10px 10px",
+            fontSize: "12px",
+          },
+          className: "font-mono uppercase font-light",       // you can add tailwind classes
+        }} position="top-center" />
         {children}
       </body>
     </html>
